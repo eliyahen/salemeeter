422941042a9a93037d00901bace0a0494f79e432
Eli Hen <elihen@tikalk.com>
2018-11-18 04:05:34 +0200

    Added schema apply to document.

---
+++ utils/schema.js
@@ -0,0 +1,44 @@
+
+/**
+ * Applies the given schema to the data.
+ * 
+ * @param {{schema, isArray}} docSchema schema definition. schema=null means anything. isArray means the schema is for each item in the array.
+ * @param {*} data the data to filter according the schema.
+ */
+const applyDocumentSchema = ({schema, isArray = false}, data) => {
+    let retData;
+
+    if (!schema || !data) {
+        retData = data;
+    } else if (isArray) {
+        retData = [];
+        if (Array.isArray(data)) {
+            retData = data.reduce((acc, item) => {
+                acc.push(applyDocumentSchema({schema}, item));
+                return acc;
+            }, retData);
+        }
+    } else if (Array.isArray(schema)) {
+        retData = {};
+        if (typeof(data) === 'object') {
+            retData = schema.reduce((acc, f) => {
+                if (typeof(f) === 'string') {
+                    f = [f, {schema: null}];
+                }
+                const [fname, fschema] = f;
+                if (fname in data) {
+                    acc[fname] = applyDocumentSchema(fschema, data[fname]);
+                }
+                return acc;
+            }, retData);
+        }
+    } else {
+        throw new Error('applyDocumentSchema: invalid schema supplied.');
+    }
+
+    return retData;
+};
+
+module.exports = {
+    applyDocumentSchema,
+};
